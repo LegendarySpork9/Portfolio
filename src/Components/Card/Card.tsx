6230864c0157c7e71ceb8f6295f5371bb6b220f0
LegendarySpork9 <tobyhunter1999@btopenworld.com>
2025-10-01 08:55:39 +0100

    Item Cards
    Created the portfolio item cards and added them to the home page.

---
+++ src/Components/Card/Card.tsx
@@ -0,0 +1,58 @@
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography';
+import Circle from '@mui/icons-material/Circle';
+import Tooltip from '@mui/material/Tooltip';
+import './Card.css';
+
+interface ItemCardProps {
+  image: string;
+  title: string;
+  status: string;
+  date: string;
+}
+
+const ItemCard = ({image, title, status, date}: ItemCardProps) => {
+  return (
+    <Card className="card" >
+      <CardActionArea className="card-action" >
+        <CardMedia
+          component="img"
+          height="200"
+          image={image}
+          alt="Item Image"
+          className="card-Media"
+        />
+
+        <CardContent>
+          <Paper className="card-paper" >
+            <Typography variant="subtitle2" color="white" >
+              {title}
+            </Typography>
+          </Paper>
+
+          <div className="card-div" style={{marginTop: "10px"}} >
+            <Tooltip className="card-tooltip" title="Issue Indicator: Green = 0, Yellow = < New Features, Red = < Bugs" arrow placement="top" >
+              <Paper className="card-paper" >
+                <Circle style={{color: status, fontSize: 14}} />
+              </Paper>
+            </Tooltip>
+
+            <Tooltip className="card-tooltip" title="Last Modified Date" arrow placement="top" >
+              <Paper className="card-paper" style={{flex: "1"}} >
+                <Typography variant="subtitle2" color="white" >
+                  {date}
+                </Typography>
+              </Paper>
+            </Tooltip>
+          </div>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+export default ItemCard;
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,27 +1,84 @@
 import Navbar from '../../Components/Navbar/Navbar'
 import RightSidebar from '../../Components/Sidebars/Right/RightSidebar'
+import Card from '../../Components/Card/Card'
 import './Home.css'
+
+const items = [
+  {
+    id: 1,
+    image: "https://raw.githubusercontent.com/LegendarySpork9/Other-Projects/refs/heads/main/NASA%20Image%20Report/NASA%20Image%20Report/Content/Logo.ico",
+    title: "NASA Image Report",
+    status: "Green",
+    date: "30 September 2025 21:20"
+  },
+  {
+    id: 2,
+    image: "https://raw.githubusercontent.com/LegendarySpork9/Other-Projects/refs/heads/main/NASA%20Image%20Report/NASA%20Image%20Report/Content/Logo.ico",
+    title: "GitHub to Codecks",
+    status: "Yellow",
+    date: "30 September 2025 21:20"
+  },
+  {
+    id: 3,
+    image: "https://raw.githubusercontent.com/LegendarySpork9/Other-Projects/refs/heads/main/NASA%20Image%20Report/NASA%20Image%20Report/Content/Logo.ico",
+    title: "Hunter Industries API",
+    status: "Red",
+    date: "30 September 2025 21:20"
+  },
+  {
+    id: 4,
+    image: "https://raw.githubusercontent.com/LegendarySpork9/Other-Projects/refs/heads/main/NASA%20Image%20Report/NASA%20Image%20Report/Content/Logo.ico",
+    title: "Server Status",
+    status: "Green",
+    date: "30 September 2025 21:20"
+  },
+  {
+    id: 5,
+    image: "https://raw.githubusercontent.com/LegendarySpork9/Other-Projects/refs/heads/main/NASA%20Image%20Report/NASA%20Image%20Report/Content/Logo.ico",
+    title: "Server Backup Tool",
+    status: "Yellow",
+    date: "30 September 2025 21:20"
+  },
+  {
+    id: 6,
+    image: "https://raw.githubusercontent.com/LegendarySpork9/Other-Projects/refs/heads/main/NASA%20Image%20Report/NASA%20Image%20Report/Content/Logo.ico",
+    title: "Database BLM Creator",
+    status: "Red",
+    date: "30 September 2025 21:20"
+  },
+  {
+    id: 7,
+    image: "https://raw.githubusercontent.com/LegendarySpork9/Other-Projects/refs/heads/main/NASA%20Image%20Report/NASA%20Image%20Report/Content/Logo.ico",
+    title: "RAG V3",
+    status: "Red",
+    date: "30 September 2025 21:20"
+  },
+  {
+    id: 8,
+    image: "https://raw.githubusercontent.com/LegendarySpork9/Other-Projects/refs/heads/main/NASA%20Image%20Report/NASA%20Image%20Report/Content/Logo.ico",
+    title: "Google Drive Sync",
+    status: "Red",
+    date: "30 September 2025 21:20"
+  }
+]
 
 function Home() {
   return (
-    <div>
+    <div className="home-container">
       <Navbar />
-      <div className="App">
-        <header className="App-header">
-          <img src="/logo.svg" className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <div className="grid-container">
+        {items.map((item) => (
+          <Card
+            key={item.id}
+            image={item.image}
+            title={item.title}
+            status={item.status}
+            date={item.date}
+          />
+        ))}
       </div>
+      
       <RightSidebar />
     </div>
   );
